d504c590cb8a6af0fefc316a471aa290eb12b2f3
Haroldo <haroldobeyerbacellar@gmail.com>
2020-12-04 18:40:25 -0300

    Adding doctor page

--- lib/pages/doctor.page.dart
+++ lib/pages/doctor.page.dart
@@ -5,9 +5,44 @@ class DoctorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: CustomNavigationBar(pos: 2,),
+      bottomNavigationBar: CustomNavigationBar(
+        pos: 2,
+      ),
       backgroundColor: Colors.purple,
-      body: Container(),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            Image.asset(
+              'assets/images/doctor.png',
+              height: 200,
+              width: 200,
+              alignment: Alignment.topCenter,
+              // alignment: Alignment.center,
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              child: Text(
+                "55 (71) 99999-9999",
+                style: TextStyle(fontSize: 24, backgroundColor: Colors.white),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              child: Icon(Icons.phone),
+              height: 150,
+              width: 350,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
